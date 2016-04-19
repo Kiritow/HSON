4c6a31618319e5bf921734c6b0d0edc4c0924713
Kirito <1362050620@qq.com>
2016-04-19 14:22:14 +0800

    Create hson_config.hpp

---
+++ hson_config.hpp
@@ -0,0 +1,25 @@
+/** HC Script Object Notation (HSON)
+*   Developed by HC TECH.
+*/
+#ifndef HC_TECH_GITHUB_PROJECT_HSON_CONFIG_HPP
+#define HC_TECH_GITHUB_PROJECT_HSON_CONFIG_HPP
+
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+namespace hson
+{
+struct pack
+{
+	string cmd;
+	map<string,string> arg;
+};
+///Declaration.
+vector<pack> getpacksfromstring(const string& str);
+}/// End of namespace hson.
+
+#include "hson.cpp"
+
+#endif /// End of HC_TECH_GITHUB_PROJECT_HSON_CONFIG_HPP
