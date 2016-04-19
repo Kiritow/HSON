3e71a5b8bda3377666061dc0e4555c04a21549f1
Kirito <1362050620@qq.com>
2016-04-19 14:24:25 +0800

    Create hson.h

---
+++ hson.h
@@ -0,0 +1,10 @@
+/** HC Script Object Notation (HSON)
+*   Developed by HC TECH.
+*/
+#ifndef HC_TECH_GITHUB_PROJECT_HSON_H
+#define HC_TECH_GITHUB_PROJECT_HSON_H
+
+#include "hson_config.hpp"
+#include "hson.cpp"
+
+#endif /// End of HC_TECH_GITHUB_PROJECT_HSON_H
