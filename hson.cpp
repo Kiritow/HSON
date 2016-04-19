a72d4a8936e68dbe5a50645e2ca234a322c1a485
Kirito <1362050620@qq.com>
2016-04-19 14:21:30 +0800

    Create hson.cpp

---
+++ hson.cpp
@@ -0,0 +1,49 @@
+/** HC Script Object Notation (HSON)
+*   Developed by HC TECH.
+*/
+
+#ifndef HC_TECH_GITHUB_PROJECT_HSON_CPP
+#define HC_TECH_GITHUB_PROJECT_HSON_CPP
+
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "main.cpp"
+
+namespace hson
+{
+vector<pack> getpacksfromstring(const string& str)
+{
+	int begin=0;
+	vector<pack> vpck;
+	while(begin<(int)str.size()&&str.find("#",begin)!=string::npos)
+	{
+		int cmdbegin=str.find("#",begin);
+		int cmdend=str.find(";",begin);
+		int cmdnameover=str.find(":",begin);
+		pack pk;
+		pk.cmd=str.substr(cmdbegin+1,cmdnameover-cmdbegin-1);
+		int databegin=cmdnameover+1;
+		while(databegin<cmdend)
+		{
+			int thisend=str.find(":",databegin);
+			if(thisend==(int)string::npos||thisend>=cmdend)
+			{
+				thisend=cmdend;
+			}
+			int sep=str.find("=",databegin);
+			string a=str.substr(databegin,sep-databegin);
+			string b=str.substr(sep+1,thisend-sep-1);
+			pk.arg[a]=b;
+			databegin=thisend+1;
+		}
+		vpck.push_back(pk);
+		begin=cmdend+1;
+	}
+	return vpck;
+}
+
+}/// End of namespace hson.
+#endif // HC_TECH_GITHUB_PROJECT_HSON_CPP
